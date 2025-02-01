1ac980d01b5d8db3aceeb40c80a4ea03e5d0cbe5
manaf12 <115929294+manaf12@users.noreply.github.com>
2025-02-01 18:38:30 +0800

    Update Navbar.jsx

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
         </div>
         {/* MOBILE LINK LIST */}
         <div
-          className={`w-screen h-screen  z-50 bg-[#e6e6ff] flex flex-col items-center  justify-start gap-8 font-medium text-lg absolute top-16 left-0 transition-all ease-in-out ${
+          className={`w-screen h-screen mt-4 z-50 bg-[#e6e6ff] flex flex-col items-center  justify-start gap-8 font-medium text-lg absolute top-16 left-0 transition-all ease-in-out ${
     open ? "opacity-100" : "opacity-0 pointer-events-none"
           }`}
         >
